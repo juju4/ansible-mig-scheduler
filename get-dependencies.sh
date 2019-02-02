ba5de517d8bfbb4cce4d6d5513c21278322d0b5a
juju4 <juju4@users.noreply.github.com>
2019-02-02 15:04:57 -0500

    galaxy lint, dependency naming, comment out mig-agent-search

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -5,7 +5,7 @@ umask 022
 
 rolesdir=$(dirname $0)/..
 
-[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 [ ! -d $rolesdir/juju4.golang ] && git clone https://github.com/juju4/ansible-golang $rolesdir/juju4.golang
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.mig-scheduler ] && ln -s ansible-mig-scheduler $rolesdir/juju4.mig-scheduler
@@ -13,4 +13,3 @@ rolesdir=$(dirname $0)/..
 
 ## don't stop build on this script return code
 true
-
